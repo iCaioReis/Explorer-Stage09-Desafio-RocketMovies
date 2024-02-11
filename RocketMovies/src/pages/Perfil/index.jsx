09926837307d7d35ac7da38dcee30e1145f0c0ca
iCaioReis <caioreis3186@gmail.com>
2024-02-10 23:22:56 -0300

    Stylized Profile page

--- RocketMovies/src/pages/Perfil/index.jsx
+++ RocketMovies/src/pages/Perfil/index.jsx
@@ -1,6 +1,6 @@
 import { FiArrowLeft, FiCamera, FiUser, FiMail, FiLock } from "react-icons/fi";
 
-import { Container } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
@@ -11,19 +11,25 @@ export function Perfil () {
                 <a href="/"><FiArrowLeft/>Voltar</a>
             </header>
 
-            <form>
-                <div>
-                    <img src="" alt="" />
-                    <button><FiCamera/></button>
-                </div>
-
+            <Form>
+                <Avatar>
+                    <img src="https://github.com/iCaioReis.png" alt="Imagem do usuário"/>
+                    <label htmlFor="avatar">
+                        <FiCamera/>
+                        <input
+                            id="avatar"
+                            type="file"
+                        />
+                    </label>
+            </Avatar>
+                
                 <Input placeholder="Nome" icon={FiUser}/>
                 <Input placeholder="E-mail" icon={FiMail}/>
                 <Input placeholder="Senha atual" icon={FiLock}/>
                 <Input placeholder="Nova senha" icon={FiLock}/>
                 <Button title={"Salvar"}/>
 
-            </form>
+            </Form>
         </Container>
     );
 }
--- RocketMovies/src/pages/Perfil/styles.js
+++ RocketMovies/src/pages/Perfil/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div `
+export const Container = styled.div`
     display: grid;
     grid-template-rows: 144px auto;
     grid-template-areas:
@@ -9,4 +9,82 @@ export const Container = styled.div `
 
     width: 100%;
     height: 100vh;
+
+    > header {
+        display: flex;
+        align-items: center;
+
+        background-color: ${({ theme }) => theme.COLORS.THEME_COLOR_500};
+        
+        padding: 0 146px;
+
+        a {
+            display: flex;
+            align-items: center;
+            gap: 8px;
+
+            color: ${({ theme }) => theme.COLORS.THEME_COLOR};
+        }
+    }
 `;
+
+export const Form = styled.form`
+        max-width: 340px;
+        margin: 0 auto;
+
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+
+        > div:nth-child(4){
+            margin-top: 16px;
+        }
+
+        > button {
+            margin-top: 16px;
+        }
+`;
+
+export const Avatar = styled.div`
+        position: relative;
+        margin: -93px auto 32px;
+
+        width: 186px;
+        height: 186px;
+
+       > img {
+            width: 186px;
+            height: 186px;
+        
+            border-radius: 50%;
+        }
+
+        > label {
+            width: 48px;
+            height: 48px;
+
+            background-color: ${({ theme }) => theme.COLORS.THEME_COLOR};
+            border-radius: 50%;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+           position: absolute;
+           right: 7px;
+           bottom: 7px;
+
+           cursor: pointer;
+
+            input {
+                display: none;
+            }
+
+            svg {
+                width: 20px;
+                height: 20px;
+
+                color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+            }
+        }
+`
