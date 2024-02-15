af4881c5adbf46a5741392637d0cbdcc77089eae
iCaioReis <caioreis3186@gmail.com>
2024-02-15 07:16:23 -0300

    Created App Routes

--- RocketMovies/src/main.jsx
+++ RocketMovies/src/main.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
+
+import theme from './styles/theme';
+
+import { Routes } from './routes';
+
 import { Home } from './pages/Home';
 import { SingIn } from './pages/SingIn';
 import { SingUp } from './pages/SingUp';
 import { MoviePreview } from './pages/MoviePreview';
 import { Profile } from './pages/Profile';
 import { CreateMovie } from './pages/CreateMovie';
-import GlobalStyles from './styles/global';
-import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <CreateMovie />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
